c95976787a7a0cc7e2ea27fe605b91e07fd2ec49
Felicia Weathers <fel.weathers@gmail.com>
2015-10-31 12:28:58 -0400

    mapkit up and working

--- CodeOwlApp/COMapViewController.h
+++ CodeOwlApp/COMapViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface COMapViewController : UIViewController
+@interface COMapViewController : UIViewController <CLLocationManagerDelegate>
 
 @end
